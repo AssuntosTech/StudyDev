13f0375e13284c975cfe500cea977e0959ce173f
tech <TechMastering@users.noreply.github.com>
2016-07-02 01:12:33 -0300

    Update Test.quiz.py

--- Test.quiz.py
+++ Test.quiz.py
@@ -8,15 +8,11 @@ print ("""
 ################################################################
 ############## Bem vindo ao Super Quiz #########################
 ################################################################
-
 Vamos a primeira pergunta!
-
 O que é mais pesado do que um kilo de pão?
-
 1 Um kilo de batata
 2 Dois de melancia
 3 Quinhentas gramas de maionese
-
 """)
 
 
@@ -26,20 +22,17 @@ if ask1 == 1:
     
     print ("Errado")
     ask = int(input("Digite 1 se for parar 2 para continuar: "))
-    if == 1:
+    if ask1 == 1:
         os.system('clear')
         print ("Tchau")
-    if == 2:
+    if ask1 == 2:
         os.system('clear')
-
         print ("""
 Vamos a segunda pergunta!
-
 Formula quimica da agua?
 1 H2O
 2 ISO
 3 CNH
-
 """)
 
         ask2 = int(input("Digite: "))
@@ -47,10 +40,10 @@ Formula quimica da agua?
             os.system('clear')
 
             print ("Errado")
-            ask = int(input("Digite 1 se for parar 2 para continuar: "))
-            if == 1:
+            ask2 = int(input("Digite 1 se for parar 2 para continuar: "))
+            if ask2 == 1:
                 os.system('clear')
                 print ("Tchau")
-            if == 2:
+            if ask2 == 2:
                 os.system('clear')
                 pass
