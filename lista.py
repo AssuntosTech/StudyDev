229c4dded6d8abfa6cd7cf11c246b234553402c5
tech <TechMastering@users.noreply.github.com>
2016-07-10 11:56:57 -0300

    Create lista.py

---
+++ lista.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+lista=[]
+for L in range(10):
+	while True:
+		try:
+			lista.append(int(input("Digite um valor: ")))
+		break
+		except:
+			print ("Digite apenas números!!!")
+
+print (lista)
