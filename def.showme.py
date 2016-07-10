6e5e503591b49fecc29f2c6ec1b6be5d548ff540
tech <TechMastering@users.noreply.github.com>
2016-07-10 13:06:47 -0300

    Update and rename lista.py to def.showme.py

---
+++ def.showme.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*- 
+
+
+# definindo uma função vazia
+def showme():
+
+	# Criando uma mensagem
+	print("Almoçar eu devo, comida eu necessito!!!")
+
+# exibindo a mensagem
+showme() 
