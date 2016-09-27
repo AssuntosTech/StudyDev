0a21ddf017c5278cdcdd4db3389b53276ceb10d4
1pRodigy <vlad.fire@hotmail.com>
2016-09-27 02:39:03 +0000

    Add files via upload

---
+++ comparandonumeros.py
@@ -0,0 +1,22 @@
+#! usr/bin/python3
+#-*- encoding:utf-8 -*-
+
+a = int(input("Primeiro número: "))
+b = int(input("Segundo número: "))
+c = int(input("Terceiro número: "))
+if a >= b and a >= c :
+	print ("O número maior é %d" %a)
+if b >= a and b >= c:
+	print ("O número maior é %d" %b)
+if c >= a and c >= b:
+	print ("O número maior é %d" %c);
+ 
+# a = int(input('a: '))
+# b = int(input('b: '))
+# c = int(input('c: '))
+# if a >= b and a >= c:
+#     print ('Maior: %d' %a)
+# elif b >= c:
+#     print ('Maior: %d' %b)
+# else:
+#     print ('Maior: %d' %c)
